ed31f46a2834ab46801680ba81d8984bf71ff71c
Zahar Statkevich <102244000+GabellaJhonson@users.noreply.github.com>
2023-01-16 06:46:51 +0300

    delete garbage

--- Patterns/Abstract Factory/checkboxes/Checkbox.java
+++
@@ -1,9 +0,0 @@
-package refactoring_guru.abstract_factory.example.checkboxes;
-
-/**
- * Чекбоксы — это второе семейство продуктов. Оно имеет те же вариации, что и
- * кнопки.
- */
-public interface Checkbox {
-    void paint();
-}
